77b96344df39abd99621bef6ae852a812cb82a20
MikhailKochetkov <consultant.dax@gmail.com>
2023-10-15 14:47:45 +0300

    Refactoring

--- db/db_connection.py
+++ db/db_connection.py
@@ -8,6 +8,6 @@ CONNECTION_STRING = (f'{os.getenv("PG_DB_URL", default="postgresql+asyncpg://")}
                      f'{os.getenv("POSTGRES_USER", default="postgres")}:'
                      f'{os.getenv("POSTGRES_PASSWORD", default="postgres")}'
                      f'@'
-                     f'{os.getenv("DB_HOST", default="localhost")}:'
+                     f'{os.getenv("DB_HOST", default="db")}:'
                      f'{os.getenv("DB_PORT", default=5432)}/'
                      f'{os.getenv("POSTGRES_DB", default="records")}')
